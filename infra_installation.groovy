d721c3b5d5e947809cbc9f5a46ed4379393d428c
shubham1507 <shubham.joshi1507@gmail.com>
2024-08-03 22:03:24 +0530

    updated

--- infra_installation.groovy
+++ infra_installation.groovy
@@ -2,12 +2,11 @@ pipeline {
     agent any
 
     environment {
-        GIT_REPO = 'https://github.com/yourusername/your-repo.git'
+        GIT_REPO = 'https://github.com/shubham1507/SRE.git'
         ANSIBLE_PLAYBOOK = 'install_tools.yml'
         INVENTORY_FILE = 'inventory.ini'
-        SSH_KEY_PATH = '/var/lib/jenkins/.ssh/id_rsa'
-        SSH_PUBLIC_KEY_PATH = '/var/lib/jenkins/.ssh/id_rsa.pub'
-        USER = 'jenkins'
+        SSH_KEY_PATH = '/var/lib/jenkins/.ssh/id_rsa.pem'
+        USER = 'ubuntu'
     }
 
     stages {
@@ -16,32 +15,14 @@ pipeline {
                 script {
                     sh """
                     if [ ! -f ${env.SSH_KEY_PATH} ]; then
-                        ssh-keygen -t rsa -b 4096 -C "${env.USER}@localhost" -f ${env.SSH_KEY_PATH} -N ""
+                        echo "Please upload your PEM file to ${env.SSH_KEY_PATH}"
                     fi
                     """
                 }
             }
         }
 
-        stage('Add Public Key to Remote Servers') {
-            steps {
-                script {
-                    def publicKey = sh(script: "cat ${env.SSH_PUBLIC_KEY_PATH}", returnStdout: true).trim()
-
-                    def commands = [
-                        "ssh -i ${env.SSH_KEY_PATH} ${env.USER}@10.0.1.217 'mkdir -p ~/.ssh && echo \"${publicKey}\" >> ~/.ssh/authorized_keys && chmod 600 ~/.ssh/authorized_keys'",
-                        "ssh -i ${env.SSH_KEY_PATH} ${env.USER}@10.0.1.11 'mkdir -p ~/.ssh && echo \"${publicKey}\" >> ~/.ssh/authorized_keys && chmod 600 ~/.ssh/authorized_keys'",
-                        "ssh -i ${env.SSH_KEY_PATH} ${env.USER}@3.110.136.0 'mkdir -p ~/.ssh && echo \"${publicKey}\" >> ~/.ssh/authorized_keys && chmod 600 ~/.ssh/authorized_keys'"
-                    ]
-
-                    commands.each { command ->
-                        sh command
-                    }
-                }
-            }
-        }
-
-        stage('Checkout Ansible Playbook') {
+        stage('Checkout Ansible Playbooks') {
             steps {
                 git url: "${env.GIT_REPO}"
             }
@@ -56,27 +37,11 @@ pipeline {
                 }
             }
         }
-
-        stage('Update Firewall Rules') {
-            steps {
-                script {
-                    def commands = [
-                        "ssh -i ${env.SSH_KEY_PATH} ${env.USER}@10.0.1.217 'sudo ufw allow 8080/tcp'",
-                        "ssh -i ${env.SSH_KEY_PATH} ${env.USER}@10.0.1.11 'sudo ufw allow 8081/tcp'",
-                        "ssh -i ${env.SSH_KEY_PATH} ${env.USER}@3.110.136.0 'sudo ufw allow 8082/tcp'"
-                    ]
-
-                    commands.each { command ->
-                        sh command
-                    }
-                }
-            }
-        }
     }
 
     post {
         always {
-            archiveArtifacts artifacts: '**/ansible.log', allowEmptyArchive: true
+            archiveArtifacts artifacts: '**/*.log', allowEmptyArchive: true
             cleanWs()
         }
     }
