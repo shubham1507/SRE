506491b00aae52c8fda1689be8be3b8a29e7f4a4
shubham1507 <shubham.joshi1507@gmail.com>
2024-08-04 19:39:55 +0530

    updated infra_installation.groovy

--- infra_installation.groovy
+++ infra_installation.groovy
@@ -3,43 +3,28 @@ pipeline {
 
     environment {
         GIT_REPO_URL = 'https://github.com/shubham1507/SRE.git'
-        INVENTORY_FILE = 'inventory.ini'
+        INVENTORY_FILE_URL = 'https://github.com/shubham1507/SRE/raw/master/inventory.ini'
         ANSIBLE_PLAYBOOK = 'install_tools.yml'
     }
 
     stages {
         stage('Checkout Code') {
             steps {
-                // Clone the Git repository containing the Ansible playbook
+                // Clone the Git repository containing the Ansible playbook and inventory file
                 git url: "${env.GIT_REPO_URL}"
-            }
-        }
-
-        stage('Prepare Inventory') {
-            steps {
-                script {
-                    // Write the inventory file dynamically
-                    writeFile file: "${env.INVENTORY_FILE}", text: '''\
-[jenkins_target]
-10.0.1.120
-
-[nexus]
-10.0.1.178
-
-[kubernetes]
-10.0.1.156
-
-[all:vars]
-ansible_user=ubuntu
-'''
-                }
             }
         }
 
         stage('Run Ansible Playbook') {
             steps {
+                script {
+                    // Download the inventory file from the specified URL
+                    sh "wget ${env.INVENTORY_FILE_URL}"
+                }
+
+                // Run Ansible playbook
                 withCredentials([sshUserPrivateKey(credentialsId: 'ubuntu_ssh', keyFileVariable: 'SSH_KEY')]) {
-                    sh "ansible-playbook -i ${env.INVENTORY_FILE} ${env.ANSIBLE_PLAYBOOK} --private-key=${SSH_KEY}"
+                    sh "ansible-playbook -i inventory.ini ${env.ANSIBLE_PLAYBOOK} --private-key=${SSH_KEY}"
                 }
             }
         }
